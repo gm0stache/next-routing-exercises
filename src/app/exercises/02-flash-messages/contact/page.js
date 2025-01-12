bdcb0f8ffd4315d8ea060c9672563c4d59975077
Gabriel <45315755+gm0stache@users.noreply.github.com>
2025-01-12 11:05:35 +0100

    feat: complete exercise no.2

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,24 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { ToastContext } from "../../../../components/ToastProvider";
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = React.useContext(ToastContext);
+
   return (
     <main>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          createToast(
+            "we received your message and will get back to you soon!",
+            "success",
+          );
+          router.push("/exercises/02-flash-messages");
+        }}
+      >
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
--- src/app/exercises/02-flash-messages/page.js
+++ src/app/exercises/02-flash-messages/page.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import ToastShelf from "../../../components/ToastShelf";
 
 function FlashMsgExercise() {
   return (
-    <main>
-      <h1>Welcome to my website!</h1>
-      <p>
-        Got feedback? Please{' '}
-        <Link href="/exercises/02-flash-messages/contact">
-          contact us
-        </Link>
-        .
-      </p>
-    </main>
+    <>
+      <main>
+        <h1>Welcome to my website!</h1>
+        <p>
+          Got feedback? Please{" "}
+          <Link href="/exercises/02-flash-messages/contact">contact us</Link>.
+        </p>
+      </main>
+      <ToastShelf />
+    </>
   );
 }
 
